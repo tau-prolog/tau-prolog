8488e85442f15fed26f9d9e36ec85726c9a1fb99
José Antonio Riaza Valverde <riazavalverde@gmail.com>
2018-07-04 23:37:52 +0200

    add acyclic_term/1 predicate

--- code/core/beta.js
+++ code/core/beta.js
@@ -3753,6 +3753,15 @@
 				}
 			},
 			
+			// acyclic_term/1
+			"acyclic_term/1": function( thread, point, atom ) {
+				var test = point.substitution.apply( point.substitution );
+				for( var i in point.substitution.links )
+					if( !point.substitution.links[i].equals( test.links[i] ) )
+						return;
+				thread.success( point );
+			},
+			
 			// IMPLEMENTATION DEFINED HOOKS
 			
 			// halt/0
