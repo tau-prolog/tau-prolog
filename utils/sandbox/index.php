7061e038b1ae6b7a5f55d9f47d1db006d2357d56
jariazavalverde <riazavalverde@gmail.com>
2018-10-23 13:14:01 +0200

    add sandbox files

---
+++ utils/sandbox/index.php
@@ -0,0 +1,166 @@
+<?php
+
+$_time_core = filemtime("../code/core-latest.js");
+$_time_lists = filemtime("../code/lists-latest.js");
+$_time_random = filemtime("../code/random-latest.js");
+$_time_statistics = filemtime("../code/statistics-latest.js");
+$_time_dom = filemtime("../code/dom-latest.js");
+$_time_js = filemtime("../code/js-latest.js");
+
+function genid() {
+	$letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	$len = strlen($letters);
+	$id = "";
+	for($i = 0; $i < 8; $i++){
+		$id .= $letters[rand(0,$len)];
+	}
+	return $id;
+}
+
+function genuniqid($link) {
+	$exists = true;
+	$id = "";
+	while( $exists ) {
+		$id = genid();
+		$exists = $link->query("SELECT id FROM code WHERE link='$id'")->num_rows > 0;
+	}
+	return $id;
+}
+
+$program = ":-use_module(library(lists)).\n";
+$title = "";
+$description = "Test your Prolog online with Tau Prolog code editor.";
+$date = "";
+
+if( isset($_GET["id"]) ) {
+	
+	require( "mysql.php" );
+	$id = $_link->escape_string($_GET["id"]);
+	$query = $_link->query("SELECT * FROM code WHERE link='$id'");
+	while( $row = $query->fetch_assoc() ) {
+		$program = $row["code"];
+		$title = $row["name"];
+		$description = $row["description"];
+		$date = $row["date"];
+		if ($title == "") $title = $id;
+		if ($description == "") $description = $id;
+		$description .= " - Tau Prolog Sandbox";
+	}
+	
+} elseif( isset($_POST["submit"]) ) {
+	
+	require( "mysql.php" );
+	if( isset($_POST["title"]) ) $title = $_link->escape_string($_POST["title"]); else $title = "";
+	if( isset($_POST["description"]) ) $description = $_link->escape_string($_POST["description"]); else $description = "";
+	$program = $_link->escape_string($_POST["program"]);
+	$id = genuniqid($_link);
+	$query = $_link->query("INSERT INTO code(link, olink, code, name, description) VALUES ('$id', '$id', '$program', '$title', '$description')");
+	header("location: $id");
+
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title><?php if( $title != "") echo $title . " - "; ?>Tau Prolog Sandbox</title>
+		<meta name="description" content="<?php echo $description; ?>" />
+		<meta name="author" content="José Antonio Riaza Valverde" />
+		<meta name="keywords" content="Prolog, JavaScript, Interpreter, Logic, Programming">
+		<meta charset="UTF-8" />
+		<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
+		<link href="/content/img/favicon.ico" type="image/x-icon" rel="icon" />
+		<link rel="StyleSheet" href="main.css" type="text/css" media="ALL" />
+		<!-- Tau Prolog modules -->
+		<script type="text/javascript" src="/code/core-latest.js?update=<?php echo $_time_core; ?>"></script>
+		<script type="text/javascript" src="/code/lists-latest.js?update=<?php echo $_time_lists; ?>"></script>
+		<script type="text/javascript" src="/code/random-latest.js?update=<?php echo $_time_random; ?>"></script>
+		<script type="text/javascript" src="/code/statistics-latest.js?update=<?php echo $_time_statistics; ?>"></script>
+		<script type="text/javascript" src="/code/dom-latest.js?update=<?php echo $_time_dom; ?>"></script>
+		<script type="text/javascript" src="/code/js-latest.js?update=<?php echo $_js_statistics; ?>"></script>
+		<!-- Tau Prolog utils -->
+		<script type="text/javascript" src="/utils/draw-tree.js"></script>
+		<!-- Codemirror -->
+		<script src="codemirror/lib/codemirror.js"></script>
+		<link rel="stylesheet" href="codemirror/lib/codemirror.css">
+		<link rel="stylesheet" href="codemirror/theme/tau.css">
+		<link rel="stylesheet" href="codemirror/theme/tauout.css">
+		<!--script src="codemirror/mode/javascript/javascript.js"></script-->
+		<script src="codemirror/addon/mode/simple.js"></script>
+		<script src="codemirror/mode/prolog/prolog.js"></script>
+		<script src="codemirror/addon/placeholder/placeholder.js"></script>
+		<script type="text/javascript" src="main.js"></script>
+	</head>
+	<body>
+		<div id="save" style="display:none;">
+			<form action="./" method="post">
+				<input id="save-title" name="title" type="input" placeholder="Title (optional)" />
+				<textarea id="save-program" style="display:none!important;" name="program"><?php echo $program; ?></textarea>
+				<textarea id="save-descr" name="description" type="input" placeholder="Description (optional)"></textarea>
+				<p id="save-legal">The code you may store and/or share through this service is your responsibility. We do not guarantee the completeness, truthfulness, accuracy or reliability of the code you post here. Use this sandbox at your own risk.</p>
+				<input id="save-submit" name="submit" type="submit" value="Save" />
+			</form>
+		</div>
+		<div id="help" style="display:none;">
+			Look at <a href="http://tau-prolog.org/documentation" title="Tau Prolog: Documentation" target="_blank">built-in predicates and modules</a> supported by Tau Prolog.
+			<ul>
+				<li><a href="http://tau-prolog.org/documentation#lists"  title="Tau Prolog: Documentation # lists module" target="_blank">lists</a> <span title="Add to your program" onClick="add(':-use_module(library(lists)).');">:-use_module(library(lists)).</span></li>
+				<li><a href="http://tau-prolog.org/documentation#random"  title="Tau Prolog: Documentation # random module" target="_blank">random</a> <span title="Add to your program" onClick="add(':-use_module(library(random)).');">:-use_module(library(random)).</span></li>
+				<li><a href="http://tau-prolog.org/documentation#statistics"  title="Tau Prolog: Documentation # statistics module" target="_blank">statistics</a> <span title="Add to your program" onClick="add(':-use_module(library(statistics)).');">:-use_module(library(statistics)).</span></li>
+				<li><a href="http://tau-prolog.org/documentation#dom"  title="Tau Prolog: Documentation # dom module" target="_blank">dom</a> <span title="Add to your program" onClick="add(':-use_module(library(dom)).');">:-use_module(library(dom)).</span></li>
+				<li><a href="http://tau-prolog.org/documentation#js"  title="Tau Prolog: Documentation # js module" target="_blank">js</a> <span title="Add to your program" onClick="add(':-use_module(library(js)).');">:-use_module(library(js)).</span></li>
+			</ul>
+			<div class="help-bar"></div>
+			<p class="help-par">Latest updates</p>
+			<ul class="help-list-version">
+				<li>core.js <?php echo "[" . $_time_core . "] " . date ("(d-m-Y H:i)", $_time_core); ?></li>
+				<li>lists.js <?php echo "[" . $_time_lists . "] " . date ("(d-m-Y H:i)", $_time_lists); ?></li>
+				<li>random.js <?php echo "[" . $_time_random . "] " . date ("(d-m-Y H:i)", $_time_random); ?></li>
+				<li>statistics.js <?php echo "[" . $_time_statistics . "] " . date ("(d-m-Y H:i)", $_time_statistics); ?></li>
+				<li>dom.js <?php echo "[" . $_time_dom . "] " . date ("(d-m-Y H:i)", $_time_dom); ?></li>
+				<li>js.js <?php echo "[" . $_time_js . "] " . date ("(d-m-Y H:i)", $_time_js); ?></li>
+			</ul>
+		</div>
+<?php if (!isset($_GET["toolbar"]) || $_GET["toolbar"] != "hidden") { ?>
+		<div id="toolbar">
+			<ul>
+					<li><a id="home" href="http://tau-prolog.org" title="Tau Prolog"></a></li>
+					<li><a href="http://tau-prolog.org/sandbox" title="New sandbox" />New</a></li>
+					<li><input type="button" value="Save" onClick="toggle('save');" title="Permanent link" /></li>
+					<li><input type="button" value="Help" onClick="toggle('help');" title="Help" /></li>
+			</ul>
+		</div>
+<?php } ?>
+		<div id="sections">
+			<ul>
+					<li><input type="button" id="program-tab" onClick="show_program_tab();" value="Program editor" class="selected-section" /></li>
+					<li><input type="button" id="derivation-tab" onClick="show_derivation_tab();" value="Derivation tree visualization" /></li>
+			</ul>
+		</div>
+		<div id="content">
+			<div id="runable">
+				<div id="program-container">
+					<div>
+						<div class="header">Program</div>
+						<div id="program"><?php echo htmlspecialchars($program); ?></div>
+					</div>
+				</div>
+				<div id="canvas-container">
+						<div class="canvas-note">Only queries executed under this tab will be drawn.</div>
+						<canvas id="tau-canvas"></canvas>
+				</div>
+				<div id="query-container">
+					<div>
+						<div class="header">Query
+							<span id="limit-container" title="Maximum number of inferences">limit: <input id="limit" type="text" value="10000" /></span>
+							<span id="max_answers-container" title="Maximum number of answers">max-answers: <input id="max_answers" type="text" value="10" /></span>
+						</div>
+						<div id="query"></div>
+						<div id="output"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ utils/sandbox/main.js
@@ -0,0 +1,157 @@
+var try_program = "";
+var try_goal = "";
+var try_goals = [""];
+var try_stack = 0;
+var session = null;
+var code = null;
+var query = null;
+var MODE_PROGRAM = 1;
+var MODE_DERIVATION = 2;
+var mode = MODE_PROGRAM;
+var reset = 0;
+
+window.addEventListener("load", function() {
+	var value = document.getElementById("program").innerHTML.replace(/&lt;/g,"<").replace(/&gt;/g,">");
+	document.getElementById("program").innerHTML = "";
+	code = CodeMirror(document.getElementById("program"), {
+		value: value,
+		lineNumbers: true,
+		theme: "tau",
+		placeholder: "Your program here...",
+		mode: "prolog"
+	});
+	query = CodeMirror(document.getElementById("query"), {
+		lineNumbers: false,
+		theme: "tauout",
+		placeholder: "Type a Prolog goal in here and press ENTER",
+		mode: "prolog"
+	});
+	code.setSize("100%", "100%");
+	code.on("change", function(instance, change) {
+		document.getElementById("save-program").value = instance.getValue();
+	});
+	query.setSize("100%", query.defaultTextHeight() + 2 * 4);
+	query.on("beforeChange", function(instance, change) {
+		var newtext = change.text.join("").replace(/\n/g, "");
+		change.update(change.from, change.to, [newtext]);
+		return true;
+	});
+	query.on("keyHandled", try_tau_prolog)
+});
+
+function try_tau_prolog( cm, msg, e ) {
+	// Down
+	if( e.keyCode === 40 ) {
+		try_stack++;
+		if( try_stack >= try_goals.length ) try_stack = 0;
+		query.setValue(try_goals[try_stack]);
+	// Up
+	} else if( e.keyCode === 38 ) {
+		try_stack--;
+		if( try_stack < 0 ) try_stack = try_goals.length - 1;
+		query.setValue(try_goals[try_stack]);
+	// Enter
+	} else if( e.keyCode === 13 ) {
+		try {
+			var raw_program = code.getValue();
+			var raw_goal = query.getValue();
+			if( try_program !== raw_program || try_goal !== raw_goal || reset || mode == MODE_DERIVATION ) {
+				if( mode == MODE_PROGRAM ) {
+					new_block(raw_goal);
+					try_goals.push( raw_goal );
+					try_stack = try_goals.length - 1;
+					reset = 0;
+				} else if( mode == MODE_DERIVATION ) {
+					reset = 1;
+				}
+				session = pl.create(parseInt(document.getElementById("limit").value));
+				var c = session.consult( raw_program );
+				var q = session.query( raw_goal );
+				if( c !== true ) {
+					try_answer( 'error parsing program: ' + c.args[0], true );
+					return;
+				}
+				if( q !== true ) {
+					try_answer( 'error parsing query: ' + q.args[0], true );
+					return;
+				}
+			}
+		} catch( ex ) {
+			try_answer( 'javascript error: ' + ex.toString() + '<div class="report-github"><a href="https://github.com/jariazavalverde/tau-prolog/issues" target="_blank">Report error on GitHub</a></div>', true );
+			return;
+		}
+		if( mode == MODE_PROGRAM )
+			session.answer( try_answer );
+		else if( mode == MODE_DERIVATION ) {
+			var max_answers = parseInt(document.getElementById("max_answers").value);
+			tau_prolog_derivation.draw("tau-canvas", session, max_answers);
+		}
+	}
+	try_program = raw_program;
+	try_goal = raw_goal;
+}
+
+function escapeHtml(unsafe) {
+	return unsafe.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+}
+
+function new_block(last) {
+	document.getElementById( "output" ).innerHTML = "<div class=\"goal\"></div>" + document.getElementById( "output" ).innerHTML;
+	document.getElementById( "output" ).innerHTML = "<div class=\"last\">?- " + last + "</div>" + document.getElementById( "output" ).innerHTML;
+}
+
+function try_answer( answer, format ) {
+	var elem = document.getElementsByClassName( "goal" )[0];
+	var inner = elem.innerHTML;
+	if( inner !== "" ) {
+		elem.innerHTML = "<div class=\"sep\"></div>" + inner;
+	}
+	elem.innerHTML = "<div class=\"answer\">" + (format ? answer : escapeHtml(pl.format_answer( answer, session )) ) + "</div>" + elem.innerHTML;	
+}
+
+function toggle(id) {
+	if( id == "save")
+		document.getElementById("help").style.display = 'none';
+	else
+		document.getElementById("save").style.display = 'none';
+	var elem = document.getElementById( id );
+	if( elem.style.display == 'none' ) {
+		elem.style.display = 'block';
+	} else {
+		elem.style.display = 'none';
+	}
+}
+
+function add(text) {
+	code.setValue(text + "\n" + code.getValue());
+}
+
+function addClassName( elem, name ) {
+	var arr = elem.className.split(" ");
+	if( arr.indexOf(name) == -1 )
+		elem.className += " " + name;
+}
+
+function removeClassName( elem, name ) {
+	elem.className = elem.className.replace(name, "");
+} 
+
+function show_program_tab() {
+	removeClassName(document.getElementById( "derivation-tab" ), "selected-section");
+	addClassName(document.getElementById( "program-tab" ), "selected-section");
+	document.getElementById( "canvas-container" ).style.display = "none";
+	document.getElementById( "program-container" ).style.display = "block";
+	document.getElementById( "output" ).style.display = "block";
+	document.getElementById( "max_answers-container" ).style.display = "none";
+	mode = MODE_PROGRAM;
+}
+
+function show_derivation_tab() {
+	removeClassName(document.getElementById( "program-tab" ), "selected-section");
+	addClassName(document.getElementById( "derivation-tab" ), "selected-section");
+	document.getElementById( "program-container" ).style.display = "none";
+	document.getElementById( "canvas-container" ).style.display = "block";
+	document.getElementById( "output" ).style.display = "none";
+	document.getElementById( "max_answers-container" ).style.display = "inline";
+	mode = MODE_DERIVATION;
+}
