61094c257d48d8179960131867a015dca0ec7b62
José Antonio Riaza Valverde <riazavalverde@gmail.com>
2020-01-27 19:56:07 +0100

    added license/0 and version/0 built-in predicates (Resolve #92)

--- modules/core.js
+++ modules/core.js
@@ -3675,7 +3675,7 @@
 			},
 		
 
-			
+
 			// LOGIC AND CONTROL STRUCTURES
 		
 			// ;/2 (disjunction)
@@ -6421,6 +6421,40 @@
 				var grbody = atom.args[0], s0 = atom.args[1];
 				thread.prepend( [new State(
 					point.goal.replace( new Term( "phrase", [grbody, s0, new Term("[]", [])] ) ), 
+					point.substitution,
+					point
+				)] );
+			},
+
+
+
+			// TAU PROLOG INFORMATION
+
+			// version/0
+			"version/0": function( thread, point, atom ) {
+				var msg = "Welcome to Tau Prolog version " + version.major + "." + version.minor + "." + version.patch + "\n";
+				msg += "Tau Prolog comes with ABSOLUTELY NO WARRANTY. This is free software.\n";
+				msg += "Please run ?- license. for legal details.\n";
+				msg += "For online help and background, visit http:/tau-prolog.org";
+				thread.prepend( [new State(
+					point.goal.replace( new Term( "write", [new Term( msg, [] )] ) ), 
+					point.substitution,
+					point
+				)] );
+			},
+
+			// license/0
+			"license/0": function( thread, point, atom ) {
+				var msg = "Tau Prolog. A Prolog interpreter in JavaScript.\n";
+				msg += "Copyright (C) 2017 - 2020 José Antonio Riaza Valverde\n\n";
+				msg += "Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:\n";
+				msg += "1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.\n";
+				msg += "2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.\n";
+				msg += "3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.\n\n";
+				msg += "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n\n";
+				msg += "You should have received a copy of the BSD 3-Clause License along with this program. If not, see https://opensource.org/licenses/BSD-3-Clause";
+				thread.prepend( [new State(
+					point.goal.replace( new Term( "write", [new Term( msg, [] )] ) ), 
 					point.substitution,
 					point
 				)] );
