55049d34c6ae3356c4ffe4bb769300376cc2cf57
José Antonio Riaza Valverde <riazavalverde@gmail.com>
2020-08-27 02:03:19 +0200

    fixed typo in flush_output/1 predicate

--- modules/core.js
+++ modules/core.js
@@ -7175,7 +7175,7 @@
 				thread.throw_error( pl.error.domain( "stream_or_alias", stream, atom.indicator ) );
 			} else if( !pl.type.is_stream( stream2 ) || stream2.stream === null ) {
 				thread.throw_error( pl.error.existence( "stream", stream, atom.indicator ) );
-			} else if( stream.input === true ) {
+			} else if( stream2.input === true ) {
 				thread.throw_error( pl.error.permission( "output", "stream", stream, atom.indicator ) );
 			} else {
 				stream2.stream.flush();
