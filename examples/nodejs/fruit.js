2627fb05fc9d4c6c3d85dec24d000bc806461467
jariazavalverde <riazavalverde@gmail.com>
2021-05-03 12:39:49 +0200

    updated fruit example

--- examples/nodejs/fruit.js
+++ examples/nodejs/fruit.js
@@ -1,24 +1,30 @@
 // Import Tau Prolog core
-// Looks like this when tau-prolog is installed with npm:
-// var pl = require("tau-prolog");
-var pl = require("../../modules/core");
+// Looks like this when tau-prolog is installed without npm:
+// var pl = require("path/to/tau-prolog/modules/core.js");
+var pl = require("tau-prolog");
 
 // Import the lists module
-// Looks like this when tau-prolog is installed with npm:
-// require("tau-prolog/modules/lists")(pl);
-require("../../modules/lists")(pl);
+// Looks like this when tau-prolog is installed without npm:
+// require("path/to/tau-prolog/modules/lists.js")(pl);
+require("tau-prolog/modules/lists")(pl);
 
 // Create a session
-var session = pl.create(1000);
+var session = pl.create();
 
 // Program
-var program =
-	// Load the lists module
-	":- use_module(library(lists))." +
-	// fruit/1
-	"fruit(apple). fruit(pear). fruit(banana)." +
-	// fruits_in/2
-	"fruits_in(Xs, X) :- member(X, Xs), fruit(X).";
+var program = `
+	:- use_module(library(lists)).
+
+	% fruit/1
+	fruit(apple).
+	fruit(pear).
+	fruit(banana).
+
+	% fruits_in/2
+	fruits_in(Xs, X) :-
+		member(X, Xs),
+		fruit(X).
+`;
 
 // Goal
 var goal = "fruits_in([carrot, apple, banana, broccoli], X).";
@@ -30,7 +36,7 @@ session.consult(program, {
 		session.query(goal, {
 			success: function() {
 				// Look for answers
-				session.answers(x => console.log(pl.format_answer(x)));
+				session.answers(x => console.log(session.format_answer(x)));
 			}
 		});
 	}
